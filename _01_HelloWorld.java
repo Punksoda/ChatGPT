2e3a498e5cece6292c25a64fbdf1409ac2ed3ffb
HoMc1343 <qre123456@gmail.com>
2025-01-31 14:51:05 +0900

    java

---
+++ _01_HelloWorld.java
@@ -0,0 +1,9 @@
+package chap_01;
+
+public class _01_HelloWorld {
+    public static void main(String[] args) {
+        // main 자동 완성
+        System.out.println("Hello, World!!!");
+        // syout 자동 완성
+    }
+}

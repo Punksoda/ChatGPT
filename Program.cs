81d3d9a04d7de639702856acd33b957dcf07de58
HoMc1343 <qre123456@gmail.com>
2025-01-31 14:45:50 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, {0}!", name);
+    }
+}
